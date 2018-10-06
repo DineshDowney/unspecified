8b1724114966b71e7a61613747de2ef55dc53dd1
sishub joshi <shisub1999@gmail.com>
2018-10-07 00:45:49 +0530

    scratch is lit

--- unspecified/wsgi.py
+++ unspecified/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unspecified.settings")
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
